9608ed6af2c51e593f4ba5e6ce4bd16684366689
bigduu <mugeng.du@qq.com>
2022-10-13 20:54:16 +0800

    modify the hint coin

--- nvim/lua/lsp/ui.lua
+++ nvim/lua/lsp/ui.lua
@@ -1,12 +1,12 @@
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  update_in_insert = true
+	virtual_text = false,
+	signs = true,
+	update_in_insert = true,
 })
 
-local signs = { Error = "❌", Warn = "⚠️", Hint = "❓", Info = "☑️" }
+local signs = { Error = "❌", Warn = "⚠️", Hint = "⚠️", Info = "☑️" }
 
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
